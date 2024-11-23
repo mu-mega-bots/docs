c304af75fe66b306ea8613852ea91c6a23a9e51c
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2024-11-23 00:29:21 -0500

    Update and rename navbar.js to scripts.js

---
+++ public/javascript/scripts.js
@@ -0,0 +1,32 @@
+// Load Navbar
+fetch('./elements/navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('navbar').innerHTML = data;
+  });
+
+// Fetch the JSON data
+let docs = {};
+fetch('../public/static/docs.json')
+  .then(response => response.json())
+  .then(data => {
+    docs = data;
+    // Load the default content (home page)
+    loadContent('home');
+  });
+
+// Function to load content dynamically from JSON
+function loadContent(page) {
+  const pageData = docs[page];
+  if (pageData) {
+    document.getElementById('content').innerHTML = `
+      <h1>${pageData.title}</h1>
+      ${pageData.content}
+    `;
+  } else {
+    document.getElementById('content').innerHTML = `
+      <h1>Page Not Found</h1>
+      <p>The page you're looking for does not exist.</p>
+    `;
+  }
+}
